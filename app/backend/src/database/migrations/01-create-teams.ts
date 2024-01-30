b1a88ef09d822ff0c5766b64dc51ce8a29b0e6f2
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2024-01-30 17:10:58 -0300

    req 1

---
+++ app/backend/src/database/migrations/01-create-teams.ts
@@ -0,0 +1,23 @@
+import {Model, QueryInterface, DataTypes} from 'sequelize';
+import { ITeam } from '../../Interfaces/teams/ITeam';
+
+export default {
+    up(queryInterface: QueryInterface) {
+        return queryInterface.createTable<Model<ITeam>>('teams', {
+            id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true
+            },
+            teamName: {
+                field: 'team_name',
+                type: DataTypes.STRING,
+                allowNull: false,
+            }
+        })
+    },
+    down(queryInterface: QueryInterface) {
+        return queryInterface.dropTable('teams')
+    }
+}
---
+++ app/backend/src/database/models/SequelizeTeam.ts
@@ -0,0 +1,34 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class SequelizeTeam extends Model<InferAttributes<SequelizeTeam>,
+InferCreationAttributes<SequelizeTeam>> {
+  declare id: CreationOptional<number>;
+
+  declare teamName: string;
+}
+
+SequelizeTeam.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  teamName: {
+    field: 'team_name',
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+  underscored: true,
+});
